4970fc673198d1d6e39573fc0835e953a0dcea78
ahmadadi <ahmadadi06@gmail.com>
2017-02-01 08:16:47 +0700

    java class

--- javaclass/app/src/main/java/ahmad/adi/javaclass/transaksi.java
+++ javaclass/app/src/main/java/ahmad/adi/javaclass/transaksi.java
@@ -1,7 +1,35 @@
 package ahmad.adi.javaclass;
 
+import java.util.ArrayList;
+
 /**
  * Created by D2J-00 on 31/01/2017.
  */
 public class transaksi {
+    private ArrayList<barang> Listbarang = new ArrayList<>();
+
+    public void addbarang (barang barang){
+        this.Listbarang.add(barang);
+    }
+
+    public int totalTransaksi(){
+        int hasil = 0;
+        for (int i=0; i< Listbarang.size(); i++){
+            hasil += Listbarang.get(i).getHarga();
+        }
+        return hasil;
+    }
+
+    public String printTransaksi(){
+        String text = "Barang yang di beli di transaksi ini adalah : \n";
+        text +="-------------------------------------------\n";
+        for (int i = 0; i< Listbarang.size(); i++){
+            text += Listbarang.get(i).toString();
+        }
+        text += "-----------------------------------------------\n";
+        text += "total pembelian" +totalTransaksi()+"\n";
+        text += "-----------------------------------------------\n";
+        return text;
+
+    }
 }
